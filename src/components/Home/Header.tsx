0a36e7cb47a20d30231cf7e8a34f1c1751fab5b0
Aravind Jaimon <email@aravindjaimon.me>
2021-11-05 20:16:05 +0530

    font awesome plugin added

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
           <div className="text-white font-poppins opacity-50 font-semibold mt-20 text-xs xxs:text-xs xs:text-xs sm:text-xs md:text-sm lg:text-xl">
             Hello,I'm
           </div>
-          <h1 className="text-red-400 font-poppins font-semibold text-2xl xxs:text-2xl xs:text-2xl sm:text-lg md:text-2xl lg:text-4xl xl:text-7xl 2xl:text-8xl">
+          <h1 className="text-red-500 font-poppins font-semibold text-2xl xxs:text-2xl xs:text-2xl sm:text-lg md:text-2xl lg:text-4xl xl:text-7xl 2xl:text-8xl">
             Aravind Jaimon
           </h1>
           <h2 className="text-white font-poppins font-semibold text-xl xxs:text-xl xs:text-xl sm:text-2xl md:text-2xl lg:text-2xl">
